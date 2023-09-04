4cb01b6f8b8e67f4bae41f096a7b973489b125de
Collins Murgor <vintoncollins702@gmail.com>
2023-09-04 13:39:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+import { useState } from 'react';
+
+export default function FeedbackForm() {
+  function handleClick() {
+    const name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
+
